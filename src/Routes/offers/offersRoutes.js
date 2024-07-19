7b2d5378b2de78f42a66603f8ba1cf3f7a4eab62
Daniel Montero Castro <salamapatela@gmail.com>
2024-07-19 14:45:28 +0200

    CReacion de estructura de carpetas y modificacion de la tabla shiping

---
+++ src/Routes/offers/offersRoutes.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { auththenticatedUser } from "../../Middlewares/auththenticatedUser.js";
+import { adminAuthenticatedUser } from "../../Middlewares/adminAuthenticatedUser.js";
+
+// Creamos el router
+export const offersRouter = express.Router();
+
+// Crear Oferta
+
+// Actualizar oferta
+
+// Eliminar oferta
+
+// Lista de Ofertas
---
+++ src/Routes/order/orderRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { auththenticatedUser } from "../../Middlewares/auththenticatedUser.js";
+import { adminAuthenticatedUser } from "../../Middlewares/adminAuthenticatedUser.js";
+
+// Creamos el router
+export const orderRouter = express.Router();
+
+// Crear orden
+
+// Actualizar orden
+
+// Eliminar orden
+
+// Lista de ordenes
+
+// Buscar ordenes
