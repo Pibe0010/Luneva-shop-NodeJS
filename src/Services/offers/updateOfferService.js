18ef12b4c6cf311a051d11fadc2cebf099806f6d
Daniel Montero Castro <salamapatela@gmail.com>
2024-11-17 12:33:39 +0100

    Actualizacion de oferta para su funcionamiento

--- src/Services/offers/updateOfferService.js
+++ src/Services/offers/updateOfferService.js
@@ -4,31 +4,26 @@ import { updateOfferModel } from "../../Models/offers/updateOfferModel.js";
 import { handleErrorService } from "../../Utils/handleError.js";
 import { notFoundError } from "../error/errorService.js";
 
-export const updateOfferService = async (ID_product, body) => {
+export const updateOfferService = async (ID_offer, body) => {
   try {
     const { discount_rate, start_date, ending_date } = body;
 
     // Buscamos la oferta
-    const offer = await searchOfferByIdModel(ID_product);
+    const offer = await searchOfferByIdModel(ID_offer);
 
     // Comprabamos si el oferta existe
-    const ID_offer = await selectOfferByIdModel(offer.ID_offer);
+    const response = await selectOfferByIdModel(offer.ID_offer);
 
     // Si existe comprobamos que es la misma oferta
-    if (!ID_offer && ID_offer.ID_offer !== offer.ID_offer) {
+    if (!response && response.ID_offer !== offer.ID_offer) {
       notFoundError("offer");
     }
 
     // Actualizo la oferta en la BD
-    await updateOfferModel(
-      offer.ID_offer,
-      discount_rate,
-      start_date,
-      ending_date
-    );
+    await updateOfferModel(ID_offer, discount_rate, start_date, ending_date);
 
     // devolvemos la oferta actualizada
-    const updatedOffer = await selectOfferByIdModel(offer.ID_offer);
+    const updatedOffer = await selectOfferByIdModel(ID_offer);
 
     return updatedOffer;
   } catch (error) {
