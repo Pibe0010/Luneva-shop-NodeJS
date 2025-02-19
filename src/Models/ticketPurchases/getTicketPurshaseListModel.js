610bd38233e68851fc0a0dfd7fad5ba9953b556d
Daniel Montero Castro <salamapatela@gmail.com>
2025-02-19 20:29:56 +0100

    Adjuntarmas datos a la lista para los tickets

--- src/Models/ticketPurchases/getTicketPurshaseListModel.js
+++ src/Models/ticketPurchases/getTicketPurshaseListModel.js
@@ -6,12 +6,15 @@ export const getTicketPurshaseListModel = async () => {
     const pool = await getPool();
 
     const [result] = await pool.query(
-      `SELECT Ticket_purchases.ID_ticket, Ticket_purchases.ID_order, Orders.ref_OR, Orders.product_amount, Orders.price, Products.name, Users.user_name, Users.last_name, Products.name, Ticket_purchases.createdAt, Ticket_purchases.updatedAt 
+      `SELECT Ticket_purchases.ID_ticket, Ticket_purchases.ID_payment, Orders.ref_OR, Orders.product_amount, Orders.product_discount, Products.name, Products.price AS product_price, Users.user_name, Users.last_name,Ticket_purchases.status, Ticket_purchases.createdAt, Payments.total_amount, Payments.iva_payments, Payments.shipment_cost, Offers.discount_rate AS offer_discount, Ticket_purchases.updatedAt 
       FROM Ticket_purchases 
-          INNER JOIN Orders  ON Ticket_purchases.ID_order = Orders.ID_order 
+          INNER JOIN Payments  ON Ticket_purchases.ID_payment = Payments.ID_payment
+          INNER JOIN Orders  ON Orders.ID_order = Payments.ID_order
           INNER JOIN Customers  ON Orders.ID_customer = Customers.ID_customer
           INNER JOIN Users  ON Customers.ID_user = Users.ID_user
-          INNER JOIN Products  ON Orders.ID_product = Products.ID_product 
+          INNER JOIN Products  ON Orders.ID_product = Products.ID_product
+          LEFT JOIN Offers  ON Products.ID_product = Offers.ID_product
+          ORDER BY Orders.ref_OR ASC
           `
     );
 
