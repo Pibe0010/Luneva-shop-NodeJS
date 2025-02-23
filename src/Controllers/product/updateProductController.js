01f10ee592c1465ff3eb59fd5374e5338c2ce6d2
Daniel Montero Castro <salamapatela@gmail.com>
2025-02-23 11:45:02 +0100

    Import modificado

--- src/Controllers/product/updateProductController.js
+++ src/Controllers/product/updateProductController.js
@@ -2,7 +2,7 @@ import { updateImgProductService } from "../../Services/product/updateImgProduct
 import { validateSchemaUtil } from "../../Utils/validateSchemaUtil.js";
 import { updateProductService } from "../../Services/product/updateProductService.js";
 import { handleErrorController } from "../../Utils/handleError.js";
-import { productUpdatedSchema } from "../../schemas/product/productUpdatedSchema.js";
+import { productUpdatedSchema } from "../../Schemas/product/productUpdatedSchema.js";
 
 export const updateProductController = async (req, res, next) => {
   try {
